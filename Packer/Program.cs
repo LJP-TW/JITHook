ec7e48b54682befcb98e7edb5dbbef66b7f1464b
LJP-TW <accr94238@gmail.com>
2022-08-23 17:01:01 +0800

    Dumping all instruction, including offset and raw opcode

--- Packer/Program.cs
+++ Packer/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 // dnlib
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
+using dnlib.IO;
+using dnlib.PE;
 
 // Ref:
 // - https://github.com/0xd4d/dnlib/blob/master/Examples/
@@ -23,49 +25,90 @@ namespace Packer
     }
     class Program
     {
-        static void Main(string[] args)
-        {
-            // Get module
-            ModuleContext modCtx = ModuleDef.CreateModuleContext();
-            ModuleDefMD module = ModuleDefMD.Load(@"./testprog.exe", modCtx);
+        static ModuleContext modCtx;
+        static ModuleDefMD module;
+        static IPEImage PEImage;
+        static DataReader reader;
 
-            // Get assembly
-            AssemblyDef asm = module.Assembly;
-            Console.WriteLine("Assembly: {0}", asm);
+        static void packMethod(TypeDef type, MethodDef method)
+        {
+            int codesize = 0;
+
+            foreach (Instruction ins in method.Body.Instructions)
+            {
+                uint offset = ((uint)PEImage.ToFileOffset(method.RVA)) + method.Body.HeaderSize + ins.Offset;
+
+                codesize += ins.GetSize();
+
+                reader.Position = offset;
+                                
+                Console.WriteLine("0x{0:x} : {1} | {2}", offset, ins, 
+                    BitConverter.ToString(reader.ReadBytes(ins.GetSize())));
+            }
+
+            Console.WriteLine("RVA {0:x}: codesize: {1:x}", method.RVA, codesize);
 
             // Add resource
             byte[] resourceData = Encoding.UTF8.GetBytes("Hello, world!");
             module.Resources.Add(new EmbeddedResource("HelloWorld", resourceData,
                 ManifestResourceAttributes.Private));
+        }
+        static void pack()
+        {
+            // List types
+            foreach (TypeDef type in module.GetTypes())
+            {
+                Console.WriteLine("Packing Type: {0}", type.FullName);
+                // List methods
+                foreach (MethodDef method in type.Methods)
+                {
+                    Console.WriteLine("Packing Method: {0}", method.FullName);
+                    packMethod(type, method);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Get module
+            modCtx = ModuleDef.CreateModuleContext();
+            module = ModuleDefMD.Load(@"./testprog.exe", modCtx);
+            PEImage = module.Metadata.PEImage;
+            reader = module.Metadata.PEImage.DataReaderFactory.CreateReader();
+
+            // Get assembly
+            AssemblyDef asm = module.Assembly;
+            Console.WriteLine("Assembly: {0}", asm);
+
+            pack();
 
             // Get entrypoint
-            MethodDef entry = module.EntryPoint;
-            TypeDef entryType = entry.DeclaringType;
-            Console.WriteLine("Entry Point Method: {0}", entry.FullName);
-            Console.WriteLine("Type of Entry Point Method: {0}", entryType.FullName);
+            // MethodDef entry = module.EntryPoint;
+            // TypeDef entryType = entry.DeclaringType;
+            // Console.WriteLine("Entry Point Method: {0}", entry.FullName);
+            // Console.WriteLine("Type of Entry Point Method: {0}", entryType.FullName);
 
             // Create a method ref to 'System.Void System.Console::WriteLine(System.String)'
-            TypeRef consoleRef = new TypeRefUser(module, "System", "Console", module.CorLibTypes.AssemblyRef);
-            MemberRef consoleWrite1 = new MemberRefUser(module, "WriteLine",
-                        MethodSig.CreateStatic(module.CorLibTypes.Void, module.CorLibTypes.String),
-                        consoleRef);
+            // TypeRef consoleRef = new TypeRefUser(module, "System", "Console", module.CorLibTypes.AssemblyRef);
+            // MemberRef consoleWrite1 = new MemberRefUser(module, "WriteLine",
+            //             MethodSig.CreateStatic(module.CorLibTypes.Void, module.CorLibTypes.String),
+            //             consoleRef);
 
             // Create test function
-            MethodDef testfunc = new MethodDefUser("packer_func",
-                MethodSig.CreateStatic(module.CorLibTypes.Void));
-            testfunc.Attributes = MethodAttributes.Private | MethodAttributes.Static |
-                MethodAttributes.HideBySig | MethodAttributes.ReuseSlot;
-            testfunc.ImplAttributes = MethodImplAttributes.IL | MethodImplAttributes.Managed;
-            entryType.Methods.Add(testfunc);
+            // MethodDef testfunc = new MethodDefUser("packer_func",
+            //     MethodSig.CreateStatic(module.CorLibTypes.Void));
+            // testfunc.Attributes = MethodAttributes.Private | MethodAttributes.Static |
+            //     MethodAttributes.HideBySig | MethodAttributes.ReuseSlot;
+            // testfunc.ImplAttributes = MethodImplAttributes.IL | MethodImplAttributes.Managed;
+            // entryType.Methods.Add(testfunc);
+            // 
+            // var ilbody = new CilBody();
+            // testfunc.Body = ilbody;
+            // ilbody.Instructions.Add(OpCodes.Ldstr.ToInstruction("Hello World!"));
+            // ilbody.Instructions.Add(OpCodes.Call.ToInstruction(consoleWrite1));
+            // ilbody.Instructions.Add(OpCodes.Ldc_I4_0.ToInstruction());
+            // ilbody.Instructions.Add(OpCodes.Ret.ToInstruction());
 
-            var ilbody = new CilBody();
-            testfunc.Body = ilbody;
-            ilbody.Instructions.Add(OpCodes.Ldstr.ToInstruction("Hello World!"));
-            ilbody.Instructions.Add(OpCodes.Call.ToInstruction(consoleWrite1));
-            ilbody.Instructions.Add(OpCodes.Ldc_I4_0.ToInstruction());
-            ilbody.Instructions.Add(OpCodes.Ret.ToInstruction());
-
-            // Get current module
+            // Get packer module
             ModuleContext packerModCtx = ModuleDef.CreateModuleContext();
             ModuleDefMD packerModule = ModuleDefMD.Load(@"./packer.exe", packerModCtx);
 
